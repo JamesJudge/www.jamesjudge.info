fd4907b463b0e079c4dcc598db350809557afb31
James Judge <jjudge.developer83@gmail.com>
2021-09-01 11:04:25 -0400

    exp test

---
+++ html/experian_test_1.php
@@ -0,0 +1,27 @@
+<?php
+
+function calcIndex($ixd){
+	if($idx < 0){
+		throw new exception("Value entered must be zero or greater.");
+	}
+	$n = 0;
+	switch($idx){
+		case 0:
+			$n = 0
+		break;
+		case 1:
+			$n = 1
+		break;
+		case 2:
+			$n = 1
+		break;
+		default:
+			$n = calcIndex($idx-1) + ($idx-2);
+	}
+
+	return $n;
+}
+
+for($i = 0; $i < 25){
+	print($i.": ".calcIndex($i)."\n");
+}
