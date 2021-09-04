c895b90f98001f6cd987e684b34b9ebb400d4585
James Judge <jjudge.developer83@gmail.com>
2021-09-04 02:24:17 -0400

    Playing with quote form layout

--- html/experian_test_1.php
+++ html/experian_test_1.php
@@ -19,12 +19,34 @@ function calcIndex($idx){
 	return $n;
 }
 
+function calcFib($idx){
+	if($idx < 0){
+		throw new exception("Value entered must be zero or greater.");
+	}
+	$n = 0;
+	switch($idx){
+		case 0:
+			$n = 0;
+		break;
+		case 1:
+			$n = 1;
+		break;
+		default:
+			
+			for($i=2; $i<=$n; $i++){
+
+			}
+	}
+
+	return $n;
+}
+
 
 
 for($i = 0; $i < 25; $i++){
-	$before = timestamp();
+	//$before = timestamp();
 	$result = calcIndex($i);
-	$after = timestamp();
-	$elapsed = intval($after) - intval($before);
-	print($before . "- " . $after . " | Index " . $i . ": " . $result . " | Time Elapsed (ms): " . $elapsed . "\n");
+	//$after = timestamp();
+	//$elapsed = intval($after) - intval($before);
+	print(" | Index " . $i . ": " . $result . " | Time Elapsed (ms): " . $elapsed . "\n");
 }
